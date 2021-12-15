59ff6569c977bc0d2a745747645afe4a752dbb74
TH658FY <victor.sanchez@ey.com>
2021-12-15 13:30:53 -0600

    hello typescript

---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+//pollyfills
+import 'core-js/features/array/flat-map'
+import 'core-js/features/map'
+import 'core-js/features/promise'
+import 'core-js/features/set'
+import 'raf/polyfill'
+import 'whatwg-fetch'
+
+import './index.css'
+
+ReactDOM.render(
+    <h1>Hello React!</h1>,
+    document.getElementById('app-root'),
+)
